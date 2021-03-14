a9b5181d696f82e3b33be26549868667d98fd1b3
catalin.voineag <catalin.voineag@onthebeach.co.uk>
2021-03-14 12:41:22 +0000

    Initial setup for CMake

---
+++ main.cpp
@@ -0,0 +1,40 @@
+# include <iostream>
+# include <adder.h>
+# include <GLFW/glfw3.h>
+
+int main() {
+	std::cout << "Hello World\n";
+
+	std::cout << add(10.5f, 10.5f) << '\n';
+	
+    GLFWwindow* window;
+    int width, height;
+
+    if( !glfwInit() )
+    {
+        fprintf( stderr, "Failed to initialize GLFW\n" );
+        exit( EXIT_FAILURE );
+    }
+
+    window = glfwCreateWindow( 300, 300, "Gears", NULL, NULL );
+    if (!window)
+    {
+        fprintf( stderr, "Failed to open GLFW window\n" );
+        glfwTerminate();
+        exit( EXIT_FAILURE );
+    }
+
+    // Main loop
+    while( !glfwWindowShouldClose(window) )
+    {
+
+        // Swap buffers
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    // Terminate GLFW
+    glfwTerminate();
+
+	return 0;
+}
---
+++ readme.md
@@ -0,0 +1,5 @@
+Configure and build cmake using the command line
+
+	./configure.sh
+	./build.sh
+	./run.sh
